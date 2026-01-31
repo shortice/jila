84e4e765bbe4cd6100b4707faf48bfbda4a9356c
Shortice <shortice@inbox.ru>
2026-01-31 13:55:24 +0500

    Remove old Android logic

--- src/components/system/android.lua
+++ src/components/system/android.lua
@@ -1,14 +1,5 @@
 ---@meta android
 
---- @return boolean
-function android_IsExternalStorageManager() end
-
-function android_RequestExternalStorageManager() end
-
----@param title string
----@param message string
-function android_PushNotification(title, message) end
-
 ---Careful! 
 ---Actual result of this function not returning value.
 ---I use custom SDL_Event for this, because this function
@@ -18,7 +9,7 @@ function android_PushNotification(title, message) end
 ---
 ---Codes:
 ---1003 - user accept request;
----1004 - user not accept request.
+---1004 - user dont accept request.
 ---
 ---@param permission string
 ---
--- src/components/system/sys_android.cpp
+++ src/components/system/sys_android.cpp
@@ -1,7 +1,6 @@
 #include "components/system/sys_android.hpp"
 #ifdef __ANDROID__
 #include "SDL3/SDL_events.h"
-#include "components/system/jila_android.hpp"
 #endif
 
 namespace Jila {
@@ -40,21 +39,6 @@ bool android_RequestPermission(std::string permission) {
 
 void bindSysAndroid(sol::state* state) {
     #ifdef __ANDROID__
-    state -> set_function(
-        "android_IsExternalStorageManager",
-        &android_IsExternalStorageManager
-    );
-
-    state -> set_function(
-        "android_RequestExternalStorageManager",
-        &android_RequestExternalStorageManager
-    );
-
-    state -> set_function(
-        "android_PushNotification",
-        &android_PushNotification
-    );
-
     state -> set_function(
         "android_RequestPermission",
         &android_RequestPermission
